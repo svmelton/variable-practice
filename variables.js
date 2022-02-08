ccb66d5821cf480be3543885031ec69264606813
sarah melton <sarah.melton1@gmail.com>
2022-02-07 22:23:54 -0500

    Add annotations

--- variables.js
+++ variables.js
@@ -1,9 +1,17 @@
+//I am adding the names of my two pets
 var pet1 = "Chimera";
 var pet2 = "Kasha";
+//I am adding my favorite food
 var favoriteFood = "sushi";
+//I am adding my pets' age
 var petAge = 8;
+//I am adding my age
 var myAge = 38;
+//This is the current year
 var currentYear = 2022;
+//This variable tells us if the person is enrolled
 var isEnrolled = true;
+//This variable teels us if someone is logged in
 var isLoggedIn = true;
+//This variable tells us if the user is sleepy
 var isSleepy = false;
